23b938b787948d9de086c46184efddb413c0dd50
Türker Kıvılcım <kivturker@gmail.com>
2022-11-14 20:14:46 +0300

    aim animations added

--- Assets/Player/Scripts/PlayerAttack.cs
+++ Assets/Player/Scripts/PlayerAttack.cs
@@ -7,11 +7,14 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField] GameObject _crosshair;
     [SerializeField] Transform _tipOfWeapon;
 
+    Animator _animator;
+
     bool _isAiming;
 
     void Awake()
     {
         _crosshair.SetActive(false);
+        _animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -27,19 +30,20 @@ public class PlayerAttack : MonoBehaviour
         {
             _isAiming = true;
             _crosshair.SetActive(true);
-            //aiming with animator
+            _animator.SetBool(nameof(_isAiming), _isAiming);
         }
         if (Input.GetMouseButtonUp(1))
         {
             _isAiming = false;
             _crosshair.SetActive(false);
+            _animator.SetBool(nameof(_isAiming), _isAiming);
         }
     }
 
     void Fire()
     {
         if (!_isAiming || !Input.GetMouseButtonDown(0) || !Physics.Raycast(_tipOfWeapon.position, _tipOfWeapon.forward, out RaycastHit hit)) { return; }
-        
+
         HumanAI human = hit.transform.GetComponentInParent<HumanAI>();
 
         if (human != null)
